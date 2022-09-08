737feb85afb9b37b7c4897c4c44687aaaa7eb7ff
brteo <matteo@meblabs.com>
2022-09-08 17:25:12 +0200

    feat: update schema with lang field

--- deploy/seed.sh
+++ deploy/seed.sh
@@ -10,7 +10,7 @@ aws cognito-idp sign-up \
 	--client-id ${CognitoAppClientID} \
 	--username admin@meblabs.com \
 	--password Testtest1! \
-	--user-attributes Name=email,Value=admin@meblabs.com Name=name,Value=Admin Name=family_name,Value=Istrator Name=custom:role,Value=admin
+	--user-attributes Name=email,Value=admin@meblabs.com Name=name,Value=Admin Name=family_name,Value=Istrator Name=custom:lang,Value=it Name=custom:role,Value=admin
 
 aws cognito-idp admin-confirm-sign-up \
 	--region ${AWS_DEFAULT_REGION} \
