688d45a5224cfa3c0958b7a19eefddc9f5a4c70d
brteo <matteo@meblabs.com>
2022-09-16 11:02:32 +0200

    feat: removed custom url

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -14,66 +14,38 @@ HostedZoneId=$(echo ${Secrets} | jq .SecretString | jq -rc . | jq -rc '.HostedZo
 aws s3 cp ${Urls} ./urls.json
 AuthUrl=$(cat urls.json | jq ".auth.${Env}" | tr -d '"')
 
+CognitoUrl=${AuthUrl}
 if [ "$Env" = "dev" ]; then
 	CognitoUrl=$(echo ${AuthUrl/__username__/$GitUsername})
+fi
 
-	# create cognito with non custom url
-	Parameters="ParameterKey=URI,ParameterValue=${URI} ParameterKey=Url,ParameterValue=${CognitoUrl} ParameterKey=SesArn,ParameterValue=${SesArn}"
-	sam build -t ./cognito.yml -b .aws-sam/cognito/ --parameter-overrides ${Parameters}
-	sam deploy \
-		--template-file .aws-sam/cognito/template.yaml \
-		--stack-name ${URI} \
-		--disable-rollback \
-		--resolve-s3 \
-		--parameter-overrides ${Parameters} --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-		--tags project=${Project} env=${Env} creator=${GitUsername}
+Parameters="ParameterKey=URI,ParameterValue=${URI} ParameterKey=Url,ParameterValue=${CognitoUrl} ParameterKey=SesArn,ParameterValue=${SesArn}"
+sam build -t ./template.yml --parameter-overrides ${Parameters}
+sam deploy \
+	--template-file .aws-sam/build/template.yaml \
+	--stack-name ${URI} \
+	--disable-rollback \
+	--resolve-s3 \
+	--parameter-overrides ${Parameters} --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+	--tags project=${Project} env=${Env} creator=${GitUsername}
 
-	CognitoDomainUrl="https://${CognitoUrl}.auth.${AWS_DEFAULT_REGION}.amazoncognito.com"
-	echo ${CognitoDomainUrl}
-
+if [ "$Env" = "dev" ]; then
 	CognitoUserPoolID=$(aws cloudformation describe-stacks --stack-name ${URI} --query "Stacks[0].Outputs[?OutputKey=='CognitoUserPoolID'].OutputValue" --output text)
 	CognitoAppClientID=$(aws cloudformation describe-stacks --stack-name ${URI} --query "Stacks[0].Outputs[?OutputKey=='CognitoAppClientID'].OutputValue" --output text)
 
 	AdministrationPath=../../Administration/
 	cp ${AdministrationPath}.env ${AdministrationPath}.env.development
 	echo "" >>${AdministrationPath}.env.development
-	echo "AWS_COGNITO_URL=${CognitoDomainUrl}" >>${AdministrationPath}.env.development
 	echo "AWS_COGNITO_USER_POOL_ID=${CognitoUserPoolID}" >>${AdministrationPath}.env.development
 	echo "AWS_COGNITO_CLIENT_ID=${CognitoAppClientID}" >>${AdministrationPath}.env.development
 
 	CpPath=../../CP/
 	echo "" >>${CpPath}.env.local
-	echo "REACT_APP_AWS_COGNITO_URL=${CognitoDomainUrl}" >>${CpPath}.env.local
 	echo "REACT_APP_AWS_COGNITO_USER_POOL_ID=${CognitoUserPoolID}" >>${CpPath}.env.local
 	echo "REACT_APP_AWS_COGNITO_CLIENT_ID=${CognitoAppClientID}" >>${CpPath}.env.local
-else
-	CognitoUrl=${AuthUrl}
 
-	#create cognito
-	Parameters="ParameterKey=URI,ParameterValue=${URI} ParameterKey=Url,ParameterValue=${CognitoUrl} ParameterKey=SesArn,ParameterValue=${SesArn} ParameterKey=AcmArn,ParameterValue=${AcmArn}"
-	sam build -t ./cognito.yml -b .aws-sam/cognito/ --parameter-overrides ${Parameters}
-	sam deploy \
-		--template-file .aws-sam/cognito/template.yaml \
-		--stack-name ${URI} \
-		--disable-rollback \
-		--resolve-s3 \
-		--parameter-overrides ${Parameters} \
-		--capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-		--tags project=${Project} env=${Env} creator=${GitUsername}
-
-	#get cloudfront distribution for cognito custom domain (in cloud formation can't retrive it)
-	CognitoCFD=$(aws cognito-idp describe-user-pool-domain --domain ${CognitoUrl} | jq -r '.DomainDescription.CloudFrontDistribution')
-	CognitoDnsURI=${URI}-dns
-
-	#create dns record for cognito custom domain
-	Parameters="ParameterKey=Url,ParameterValue=${CognitoUrl} ParameterKey=CognitoCFD,ParameterValue=${CognitoCFD} ParameterKey=HostedZoneId,ParameterValue=${HostedZoneId}"
-	sam build -t ./cognito_dns.yml -b .aws-sam/cognito_dns/ --parameter-overrides ${Parameters}
-	sam deploy \
-		--template-file .aws-sam/cognito_dns/template.yaml \
-		--stack-name ${CognitoDnsURI} \
-		--disable-rollback \
-		--resolve-s3 \
-		--parameter-overrides ${Parameters} \
-		--capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-		--tags project=${Project} env=${Env} creator=${GitUsername}
+	AppPath=../../App/
+	echo "" >>${AppPath}.env.local
+	echo "REACT_APP_AWS_COGNITO_USER_POOL_ID=${CognitoUserPoolID}" >>${AppPath}.env.local
+	echo "REACT_APP_AWS_COGNITO_CLIENT_ID=${CognitoAppClientID}" >>${AppPath}.env.local
 fi
--- deploy/destroy.sh
+++ deploy/destroy.sh
@@ -19,25 +19,6 @@ if [ "$Env" = "production" ]; then
 fi
 
 # COGNITO
-if [ "$Env" != "dev" ]; then
-	# destroy cognito dns if not dev env
-	CognitoDnsURI=${URI}-dns
-	sam delete \
-		--stack-name ${CognitoDnsURI} \
-		--no-prompts \
-		--region ${AWS_DEFAULT_REGION}
-
-	# manual delete user pool custom domain before remove cognito
-	aws s3 cp ${Urls} ./urls.json
-	AuthUrl=$(cat urls.json | jq ".auth.${Env}" | tr -d '"')
-	UserPoolId=$(aws cognito-idp describe-user-pool-domain --domain ${AuthUrl} --region ${AWS_DEFAULT_REGION} --profile ${AWS_PROFILE} | jq -r '.DomainDescription.UserPoolId')
-	aws cognito-idp delete-user-pool-domain \
-		--domain ${AuthUrl} \
-		--user-pool-id ${UserPoolId} \
-		--region ${AWS_DEFAULT_REGION} \
-		--profile ${AWS_PROFILE}
-fi
-
 sam delete \
 	--stack-name ${URI} \
 	--no-prompts \
